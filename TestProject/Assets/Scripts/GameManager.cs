f6811db47a3ac8303b6c10637d57eb15ecb58baf
BeastlyKoboi <itsallanflores06@gmail.com>
2023-09-29 01:17:25 -0400

    Added basic time to game manager

--- TestProject/Assets/Scripts/GameManager.cs
+++ TestProject/Assets/Scripts/GameManager.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private float cash; 
 
+    // UI Elements
+    [SerializeField] private TextMeshProUGUI cashUI;
+    [SerializeField] private TextMeshProUGUI clockUI;
+
+    // Gameplay Variables 
+    [SerializeField] private float _cash;
+    private bool isPaused = false; 
+    [SerializeField] private int currentDay = 1;
+    [SerializeField] private int currentHour = 9;
+    [SerializeField] private float currentTime = 0.0f;
+    [SerializeField] private float hourLength = 5.0f;
+
+    [SerializeField] private List<GameObject> currentCustomers = new List<GameObject>();
+
+    // Properties 
     public float Cash
     {
-        get { return cash; }
-        set { cash = value; }
+        get { return _cash; }
+        set
+        {
+            _cash = value;
+            // cashUI.text = $"${_cash}"; 
+        }
     }
-
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +40,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Update the clock
+        currentTime += Time.deltaTime;
+        if (currentTime >= hourLength)
+        {
+            currentHour++;
+            currentTime -= hourLength;
+            // clockUI.text = $"{currentHour}:00";
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+            Time.timeScale = 0.0f;
+        else
+            Time.timeScale = 1.0f;
     }
 }
