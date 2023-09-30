5fd14b6580d61edeab1fb290763b3f69c7631fcb
Liam6233 <liamcurley6233@icloud.com>
2023-09-30 14:11:10 -0400

    changed piizza script to plate script

--- TestProject/Assets/Scripts/Plate.cs
+++ TestProject/Assets/Scripts/Plate.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pizza : FoodItem 
+public class Plate: MonoBehaviour
 {
     CoreIngredient[] coreFoodlist;
+
+    public Plate()
+    {
+        coreFoodlist = new CoreIngredient[3];
+    }
     // Start is called before the first frame update
     void Start()
     {
