46d9df73fc847e42bd24d9cb8cab14f6537c6346
TheMisteray <97609646+TheMisteray@users.noreply.github.com>
2023-09-29 21:59:56 -0400

    Create script shells for customers

---
+++ TestProject/Assets/Scripts/CustomerManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This script will manage all of the customers and keep track of them
+        //It may also contain methods to allow customers to communicate with each other
+        //List of customers so they can stand in a line
+        //Not needed for this sprint so this will be blank for now
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
