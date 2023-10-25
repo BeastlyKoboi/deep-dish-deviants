d32df0eba508bd35b6b1ee7bd563580d2cd3b242
TheMisteray <97609646+TheMisteray@users.noreply.github.com>
2023-10-24 20:59:19 -0400

    Opps I saved an invalid script, this should fix that

--- ChicagoStyleSorcery/Assets/Scripts/SnitchCustomer.cs
+++ ChicagoStyleSorcery/Assets/Scripts/SnitchCustomer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnitchCustomer : MonoBehaviour
+public class SnitchCustomer : Customer
 {
     // Start is called before the first frame update
     void Start()
